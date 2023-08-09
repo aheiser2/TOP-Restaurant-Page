a2c0bd3a05ad47804cce337dc011ad260f406bfe
Abby Heiser <aheiser2@yahoo.com>
2023-08-08 19:38:53 -0500

    Create Navbar with JS, creating tabs to use for navigation

--- src/navBar.js
+++ src/navBar.js
@@ -1,7 +1,33 @@
+
+const links = ["Home", "About", "Products", "Contacts"]
+
 const navBar = document.createElement('div')
+    navBar.id = "menu"
     navBar.style.backgroundColor = "green"
     navBar.style.height = "120px"
     navBar.style.width = "200px"
-    navBar.innerHTML = "I'm an export"
+    // navBar.innerHTML = "I'm an export"
+    navBar.style.border = "double purple 2px"
+    let list = document.createElement("ul")
+    navBar.appendChild(list)
 
+    for(let i = 0; i<= 3; i++){
+        let tab = document.createElement("li")
+        tab.id = i
+        tab.classList.add("menuitem")
+        tab.style.backgroundColor = "purple"
+        tab.style.height = "10px"
+        tab.style.width = "150px"
+        // tab.innerHTML = links[i]
+        let anchor = document.createElement('a')
+        tab.appendChild(anchor)
+        anchor.innerHTML = links[i]
+        anchor.href = links[i]
+        list.appendChild(tab)
+    }
+
+    // document.getElementById("0").style.backgroundColor = "white"
+
+
+    //li class="menuitem"><a href="#">Home</a></li>
     export default navBar
